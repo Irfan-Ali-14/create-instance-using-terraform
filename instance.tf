2f32609a8a7f03b8033ca2bac0a4a15cc7acf31f
Irfan Balti <irfanjaffari14@gmail.com>
2023-12-15 16:02:30 +0500

    removed ssh key

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "key-tf" {
   key_name   = "key-tf"
-  public_key = file("${path.module}/your_generated_key.pub")
+  #public_key = file("${path.module}/your_generated_key.pub")
 }
 
 
